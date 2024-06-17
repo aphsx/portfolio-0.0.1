b37a0d1a7b2e126000945c579f77a63a85a2db64
aphsx <168500857+aphsx@users.noreply.github.com>
2024-06-17 19:50:24 +0700

    new text animation

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,18 @@
-  document.addEventListener("DOMContentLoaded", () => {
-    const textElement = document.getElementById("animated-text");
-    const originalText = textElement.textContent;
-    let index = 0;
+//   document.addEventListener("DOMContentLoaded", () => {
+//     const textElement = document.getElementById("animated-text");
+//     const originalText = textElement.textContent;
+//     let index = 0;
 
-    setInterval(() => {
-        textElement.textContent = originalText.substring(0, index);
-        index = (index + 1) % (originalText.length + 1); // Loop index
-    }, 200); // Change 200 to adjust the speed (milliseconds per character)
+//     setInterval(() => {
+//         textElement.textContent = originalText.substring(0, index);
+//         index = (index + 1) % (originalText.length + 1); // Loop index
+//     }, 200); // Change 200 to adjust the speed (milliseconds per character)
+// });
+var typed = new Typed(".animated-text",{
+  strings: ["APHISIT DANCHAODANG"],
+  typeSpeed: 100,
+  backSpeed: 55,
+  loop: true,
+  showCursor: false
 });
-
   
